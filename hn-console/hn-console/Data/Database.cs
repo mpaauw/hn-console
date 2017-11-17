8f4db2bd25f2143fa5dbac6ce253738499f8a464
mpaauw <matt.paauw@gmail.com>
2017-11-16 18:30:58 -0800

    Added data namespace.

---
+++ hn-console/hn-console/Data/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hn_console.Data
+{
+    class Database : IDatabase
+    {
+        public void GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
