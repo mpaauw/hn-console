43e5db49018f6f38289be4a712e489c1de6e87af
mpaauw <matt.paauw@gmail.com>
2017-11-17 10:29:05 -0800

    Simplified various object fields in Item type class.

--- hn-console/hn-console/Model/Item.cs
+++ hn-console/hn-console/Model/Item.cs
@@ -1,4 +1,6 @@
-﻿namespace hn_console.Model
+﻿using System.Collections.Generic;
+
+namespace hn_console.Model
 {
     public class Item
     {
@@ -12,6 +14,7 @@
         public int parent { get; set; }
         public int poll { get; set; }
         public int[] kids { get; set; }
+        public List<Item> children { get; set; }
         public string url { get; set; }
         public int score { get; set; }
         public string title { get; set; }
