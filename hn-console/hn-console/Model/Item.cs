de9217359ebe31a5739e7fb02a7cf6ed28152708
mpaauw <matt.paauw@gmail.com>
2017-11-17 09:43:55 -0800

    Created Model namespace to be used for data types.

---
+++ hn-console/hn-console/Model/Item.cs
@@ -0,0 +1,21 @@
+﻿namespace hn_console.Model
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public bool deleted { get; set; }
+        public string type { get; set; }
+        public string by { get; set; }
+        public string time { get; set; } // Unix time string
+        public string text { get; set; }
+        public bool dead { get; set; }
+        public int parent { get; set; }
+        public int poll { get; set; }
+        public int[] kids { get; set; }
+        public string url { get; set; }
+        public int score { get; set; }
+        public string title { get; set; }
+        public int[] parts { get; set; }
+        public int descendents { get; set; }
+    }
+}
---
+++ hn-console/hn-console/Model/ItemType.cs
@@ -0,0 +1,12 @@
+﻿namespace hn_console.Model
+{
+    public struct ItemType
+    {
+        public const string job = "job";
+        public const string story = "story";
+        public const string comment = "comment";
+        public const string poll = "poll";
+        public const string pollopt = "pollopt";
+    }
+
+}
