4e47ce11db08c9b9f401989f424a23d7d637b0b9
mpaauw <matt.paauw@gmail.com>
2017-11-20 11:14:29 -0800

    Added Domain namespace which contains business logic files; added HtmlHelper class.

---
+++ hn-console/hn-console/Domain/HtmlHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hn_console.Domain
+{
+    public class HtmlHelper
+    {
+        public static string SanitizeHtml(string html)
+        {
+            html = html.Replace("<p>", "\n");
+            html = html.Replace("&#x27;", "'");
+            html = html.Replace("&#x2F;", "/");
+            html = html.Replace("&gt;", ">");
+            html = html.Replace("&quot;", "\"");
+
+            return html;
+        }
+    }
+}
