347314196104829f4e4ca8044c5ad089ebfab12a
mpaauw <matt.paauw@gmail.com>
2017-11-28 18:59:07 -0800

    Added additional sanitization logic.

--- hn-console/hn-console/Domain/QuickHelper.cs
+++ hn-console/hn-console/Domain/QuickHelper.cs
@@ -15,7 +15,20 @@ namespace hn_console.Domain
                 .Replace("&#x2F;", "/")
                 .Replace("&gt;", ">")
                 .Replace("&quot;", "\"")
-                .Replace("\n", " ");
+                .Replace("\n", " ")
+                .Replace("<i>", "~")
+                .Replace("</i>", "~");
+
+            while(html.Contains("<a href="))
+            {
+                int openingTagIndex = html.IndexOf("<a href=");
+                int closingTagIndex = html.IndexOf("</a>");
+                string hrefSubstring = html.Substring(openingTagIndex, (closingTagIndex - openingTagIndex) + 4);
+                string[] hrefProps = hrefSubstring.Split('"');
+                string link = hrefProps[1];
+                html = html.Replace(hrefSubstring, String.Format("[{0}]", link));
+            }
+
             return html;
         }
 
